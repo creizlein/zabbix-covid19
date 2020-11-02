8cd78863bb7828f11c29ed857c5806497a2a87a7
creizlein <creizlein@gmail.com>
2020-11-01 22:46:48 -0300

    Better handle new worldometers reponse

--- covid19_affectedcountries.py
+++ covid19_affectedcountries.py
@@ -2,28 +2,27 @@
 #===============================================================================
 # IDENTIFICATION DIVISION
 #        ID SVN:   $Id$
-#          FILE:  covid9_status.py
+#          FILE:  covid19_affectedcountries.py
 #         USAGE:  $0
 #   DESCRIPTION:  Get Worldometers Coronavirus / Covid9 affected contry list
 #  REQUIREMENTS:  pandas (pip install pandas)
 #                 lxml (pip install lxml)
-#                 beautifulsoup4 (pip install beautifulsoup4)
 #          BUGS:  ---
 #         NOTES:  ---
 #          TODO:  ---
 #        AUTHOR:  Ricardo Barbosa, rickkbarbosa@live.com
 #       COMPANY:  ---
-#       VERSION:  1.0
+#       VERSION:  1.1
 #       CREATED:  2020-Mar-10 08:57 BRT
-#      REVISION:  2020-Mar-20 09:54 BRT
+#      REVISION:  2020-Nov-01 By creizlein
 #===============================================================================
 
 import sys
 import os
+import re
 import pandas as pd
 import json
 import requests
-from bs4 import BeautifulSoup
 
 if(sys.version_info.major>=3):
     import importlib
@@ -36,16 +35,12 @@ here = os.path.dirname(os.path.abspath(__file__))
 url = 'https://www.worldometers.info/coronavirus/#countries'
 header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36","X-Requested-With": "XMLHttpRequest"}
 
-r = requests.get(url, headers=header)
+r = requests.get(url, headers=header).text
+r = re.sub(r'.*(<table id="main_table_countries_today".*?>.*?<\/table>).*', r'\1', r, flags=re.DOTALL)
+r = re.sub(r'<tbody class="body_continents">.*?<\/tbody>', '', r, flags=re.DOTALL)
+r = re.sub(r'<.*?>', lambda g: g.group(0).upper(), str(r))
 
-# fix HTML multiple tbody
-soup = BeautifulSoup(r.text, "html.parser")
-for body in soup("tbody"):
-    body.unwrap()
-
-df_countries = pd.read_html(str(soup), flavor="bs4")[0]
-
-
+df_countries = pd.read_html(r, flavor="bs4")[0]
 df_iban = pd.read_csv("{}/{}".format(here, "iban.csv"))
 result = pd.merge(df_countries,df_iban,left_on='Country,Other',right_on='Country',how='inner')
 
--- covid19_status.py
+++ covid19_status.py
@@ -7,35 +7,33 @@
 #   DESCRIPTION:  Get Worldometers Coronavirus / Covid9 data and print to JSON
 #  REQUIREMENTS:  pandas (pip install pandas)
 #                 lxml (pip install lxml)
-#                 beautifulsoup4 (pip install beautifulsoup4)
 #          BUGS:  ---
 #         NOTES:  ---
 #          TODO:  ---
 #        AUTHOR:  Ricardo Barbosa, rickkbarbosa@live.com
 #       COMPANY:  ---
-#       VERSION:  1.0
+#       VERSION:  1.1
 #       CREATED:  2020-Mar-10 21:47 BRT
-#      REVISION:  2020-Mar-20 09:33 BRT
+#      REVISION:  2020-Nov-01 By creizlein
 #===============================================================================
 
+import re
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
 
 url = 'https://www.worldometers.info/coronavirus/#countries'
 header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36","X-Requested-With": "XMLHttpRequest"}
 
-r = requests.get(url, headers=header)
+r = requests.get(url, headers=header).text
+r = re.sub(r'.*(<table id="main_table_countries_today".*?>.*?<\/table>).*', r'\1', r, flags=re.DOTALL)
+r = re.sub(r'<tbody class="body_continents">.*?<\/tbody>', '', r, flags=re.DOTALL)
+r = re.sub(r'<.*?>', lambda g: g.group(0).upper(), str(r))
 
-# fix HTML multiple tbody
-soup = BeautifulSoup(r.text, "html.parser")
-for body in soup("tbody"):
-    body.unwrap()
-
-df = pd.read_html(str(soup), index_col=1, thousands=r',', flavor="bs4")[0]
+df = pd.read_html(r, index_col=1, thousands=r',', flavor="bs4")[0]
 df = df.replace(regex=[r'\+', r'\,'], value='')
 
 df = df.fillna('0')
 df = df.to_json(orient='index')
 
 print(df)
+
